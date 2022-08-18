b62e261e4325e1e56637d882a7c53e800cec41a2
BlackSpritee <38357908+BlackSpritee@users.noreply.github.com>
2022-08-18 16:59:38 +0800

    Add files via upload

---
+++ K-BERT-master/uer/utils/constants.py
@@ -0,0 +1,22 @@
+# -*- encoding:utf-8 -*-
+from __future__ import unicode_literals
+
+# Special token ids.
+PAD_ID = 0
+UNK_ID = 100
+CLS_ID = 101
+SEP_ID = 102
+MASK_ID = 103
+
+# Special token words.
+PAD_TOKEN = '[PAD]'
+UNK_TOKEN = '[UNK]'
+CLS_TOKEN = '[CLS]'
+SEP_TOKEN = '[SEP]'
+MASK_TOKEN = '[MASK]'
+ENT1S_TOKEN='SUB0'
+ENT1E_TOKEN='SUB1'
+ENT2S_TOKEN='OBJ0'
+ENT2E_TOKEN='OBJ1'
+
+
